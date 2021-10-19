9ccaaeb5d8088295b01787d3eb7b59a72cdf86d9
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-10-20 01:21:15 +0300

    js file3

--- task1.js
+++ task1.js
@@ -27,4 +27,18 @@ function im(){
 
 
 // Change an image when the mouse moves onto it, and then back to the original image when the mouse moves off of it.
- 
+function bigImg(img1) {
+    img1.setAttribute('src' ,'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg')
+  }
+  
+  function normalImg(img2) {
+    img2.setAttribute('src' ,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0PR2ZAzwhWY7orX3aNxJE67X5TaAjAN7H_g&usqp=CAU');
+  }
+
+
+  function i1 (m){
+      m.setAttribute ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT8d87diHjkqTnE1LOdVDSonAX5kAcIDqF_zGWm7_aKm4t6g9UjDxzirAi0bIa9zItkEk&usqp=CAU" )
+      onclick="alert('This picture...')"
+      }
+
+
